65aba305dc9fa8b37426f1be1bd917596cb9f7dd
Sonsehwan45 <sahwan458@gmail.com>
2025-10-24 12:57:50 +0900

    work

---
+++ src/main/kotlin/com/back/Kp20251024Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kp20251024Application
+
+fun main(args: Array<String>) {
+    runApplication<Kp20251024Application>(*args)
+}
---
+++ src/test/kotlin/com/back/Kp20251024ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kp20251024ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
